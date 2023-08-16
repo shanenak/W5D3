ca08d2e62566d8bac41256b8254319565e902f91
David <2018dpollack@gmail.com>
2023-08-16 14:15:40 -0700

    finish self.find_by_id

--- practice-for-ch-sql-aa-questions-long-practice-main/questions.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/questions.rb
@@ -1,5 +1,6 @@
 require 'sqlite3'
 require 'singleton'
+require 'byebug'
 
 class QuestionsDBConnection < SQLite3::Database
     include Singleton
@@ -12,10 +13,35 @@ end
 
 class User
     def self.find_by_id(id)
-        QuestionsDBConnection.instance.execute(<<-SQL, id)
+        # fname = QuestionsDBConnection.instance.execute(<<-SQL, id)
+        #     SELECT fname
+        #     FROM users
+        #     WHERE id = ?;
+        # SQL
+        # lname = QuestionsDBConnection.instance.execute(<<-SQL, id)
+        #     SELECT lname
+        #     FROM users
+        #     WHERE id = ?;
+        # SQL
+        # User.new({'id' => id, 'fname' => fname, 'lname' => lname})
+        data = QuestionsDBConnection.instance.execute(<<-SQL, id)
             SELECT * 
             FROM users
-            WHERE id = ?; 
+            WHERE id = ?;
         SQL
+        debugger
+        data.map{|datum| User.new(datum)}
+    end
+
+    def initialize(options) #array of all attributes
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
     end
 end
+
+        # QuestionsDBConnection.instance.execute(<<-SQL, id)
+        #     SELECT * 
+        #     FROM users
+        #     WHERE id = ?; 
+        # SQL
