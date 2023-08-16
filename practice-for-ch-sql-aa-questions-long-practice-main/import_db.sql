fae3e281cb1ea70f1353291668a24cb0f040265e
Shannon Millar <shanenak@gmail.com>
2023-08-16 12:06:43 -0700

    Finish creating tables. Data to be added

---
+++ practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
@@ -0,0 +1,42 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    reference_id INTEGER, 
+    user_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    FOREIGN KEY (reference_id) REFERENCES replies(id)
+);
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL, --- may not be null ---- 
+    question_id INTEGER NOT NULL, ---- ditto ---- 
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
