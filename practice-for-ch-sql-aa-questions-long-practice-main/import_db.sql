f1f3097640a047c078fcd3363ed9fd9fd70ffddf
David <2018dpollack@gmail.com>
2023-08-16 12:20:39 -0700

    seeded the database

--- practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
+++ practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
@@ -40,3 +40,32 @@ CREATE TABLE question_likes (
     FOREIGN KEY (user_id) REFERENCES users(id)
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+INSERT INTO 
+    users(fname, lname)
+VALUES
+    ('Shannon', 'Millar')
+    ('David', 'Pollack');
+
+INSERT INTO
+    questions(title, body, user_id)
+VALUES
+    ('career', 'How do I get a job?', (SELECT id FROM users WHERE fname = 'Shannon'))
+    ('cat', 'Should I get a 3rd cat?', (SELECT id FROM users WHERE fname = 'David'));
+
+INSERT INTO
+    question_follows(user_id, question_id)
+VALUES
+    ((SELECT id FROM users WHERE fname = 'Shannon'), (SELECT id FROM questions WHERE title = 'cat'));
+
+INSERT INTO 
+    replies(question_id, reference_id, user_id, body)
+VALUES
+    ((SELECT id FROM questions WHERE title = 'career'), ,(SELECT id FROM users WHERE fname = 'David'), 'Join App Academy!')
+    ((SELECT id FROM questions WHERE title = 'career'),(SELECT id FROM replies WHERE body = 'Join App Academy!'),(SELECT id FROM users WHERE fname = 'Shannon'), 'Kiss up XD');
+
+INSERT INTO 
+    question_likes(user_id, question_id)
+VALUES
+    ((SELECT id FROM users WHERE fname = 'David'), (SELECT id FROM questions WHERE title = 'career'));
+
