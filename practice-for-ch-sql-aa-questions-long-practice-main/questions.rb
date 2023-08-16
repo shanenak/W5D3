2a304dfddb6fe8b20064f6a06a6ecfd66c41027f
Shannon Millar <shanenak@gmail.com>
2023-08-16 14:31:37 -0700

    Finish Reply class

--- practice-for-ch-sql-aa-questions-long-practice-main/questions.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/questions.rb
@@ -12,24 +12,23 @@ class QuestionsDBConnection < SQLite3::Database
 end
 
 class User
+    attr_accessor :id, :fname, :lname
     def self.find_by_id(id)
-        # fname = QuestionsDBConnection.instance.execute(<<-SQL, id)
-        #     SELECT fname
-        #     FROM users
-        #     WHERE id = ?;
-        # SQL
-        # lname = QuestionsDBConnection.instance.execute(<<-SQL, id)
-        #     SELECT lname
-        #     FROM users
-        #     WHERE id = ?;
-        # SQL
-        # User.new({'id' => id, 'fname' => fname, 'lname' => lname})
         data = QuestionsDBConnection.instance.execute(<<-SQL, id)
             SELECT * 
             FROM users
             WHERE id = ?;
         SQL
-        debugger
+        data.map{|datum| User.new(datum)}
+    end
+
+    def self.find_by_name(fname, lname)
+        data = QuestionsDBConnection.instance.execute(<<-SQL, fname, lname)
+            SELECT * 
+            FROM users
+            WHERE fname = ?
+            AND lname = ?;
+        SQL
         data.map{|datum| User.new(datum)}
     end
 
@@ -40,8 +39,66 @@ class User
     end
 end
 
-        # QuestionsDBConnection.instance.execute(<<-SQL, id)
-        #     SELECT * 
-        #     FROM users
-        #     WHERE id = ?; 
-        # SQL
+class Question
+    attr_accessor :id, :title, :body, :user_id
+    def self.find_by_id(id)
+        data = QuestionsDBConnection.instance.execute(<<-SQL, id)
+            SELECT * 
+            FROM questions
+            WHERE id = ?;
+        SQL
+        data.map{|datum| Question.new(datum)}
+    end
+
+    def self.find_by_title(title)
+        data = QuestionsDBConnection.instance.execute(<<-SQL, title)
+            SELECT * 
+            FROM questions
+            WHERE title = ?;
+        SQL
+        data.map{|datum| Question.new(datum)}
+    end
+
+    def self.find_by_author_id(author_id)
+        data = QuestionsDBConnection.instance.execute(<<-SQL, author_id)
+            SELECT *
+            FROM questions
+            WHERE user_id = ?;
+        SQL
+        data.map{|datum| Question.new(datum)}
+    end
+
+    def initialize(options) #array of all attributes
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @user_id = options['user_id']
+    end
+end
+
+class Reply
+    attr_accessor :id, :question_id, :reference_id, :user_id, :body
+    def initialize(options)
+        @id = options['id']
+        @question_id = options['question_id']
+        @reference_id = options['reference_id']
+        @user_id = options['user_id']
+        @body = options['body']
+    end
+    def self.find_by_user_id(user_id)
+        data = QuestionsDBConnection.instance.execute(<<-SQL, user_id)
+            SELECT * 
+            FROM replies
+            WHERE user_id = ?;
+        SQL
+        data.map {|datum| Reply.new(datum)}
+    end
+    def self.find_by_question_id(question_id)
+        data = QuestionsDBConnection.instance.execute(<<-SQL, question_id)
+            SELECT *
+            FROM replies
+            WHERE question_id = ?;
+        SQL
+        data.map {|datum| Reply.new(datum)}
+    end
+end
