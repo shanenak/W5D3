7e667e0e08df3dae7ef4068d4bdb21195fa80543
David <2018dpollack@gmail.com>
2023-08-16 15:39:05 -0700

    Finished medium questions

--- practice-for-ch-sql-aa-questions-long-practice-main/questions.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/questions.rb
@@ -45,6 +45,10 @@ class User
     def authored_replies
         Reply.find_by_user_id(self.id)
     end
+
+    def followed_questions
+        QuestionFollow.followed_questions_for_user_id(self.id)
+    end
 end
 
 class Question
@@ -89,6 +93,10 @@ class Question
 
     def replies
         Reply.find_by_question_id(self.id)
+    end
+
+    def followers
+        QuestionFollow.followers_for_question_id(self.id)
     end
 end
 
@@ -144,5 +152,37 @@ class Reply
     end
     def child_replies
         Reply.find_by_reference_id(self.id)
+    end
+end
+
+class QuestionFollow
+    attr_accessor :id, :user_id, :question_id
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
+
+    def self.followers_for_question_id(question_id)
+        data = QuestionsDBConnection.instance.execute(<<-SQL, question_id)
+            SELECT users.id, users.fname, users.lname
+            FROM users
+            JOIN question_follows 
+            ON question_follows.user_id = users.id
+            WHERE question_follows.question_id = ?;
+        SQL
+        data.map{|datum| User.new(datum)}
+    end
+
+    def self.followed_questions_for_user_id(user_id)
+        data = QuestionsDBConnection.instance.execute(<<-SQL, user_id)
+            SELECT questions.id, questions.title, questions.body, questions.user_id
+            FROM questions
+            JOIN question_follows
+            ON question_follows.question_id = questions.id
+            WHERE question_follows.user_id = ?;
+        SQL
+        data.map{|datum| Question.new(datum)}
     end
 end
